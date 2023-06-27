98b27eb1a7161d27c2bcb67b6460f05e95b9bde5
fabrizioCuellar <89095506+fabrizioCuellar@users.noreply.github.com>
2023-06-26 20:15:41 -0500

    Backend Semana 15

---
+++ PcBuildersBackendSemana15/PcBuilders/Security/Domain/Services/Communication/RoleResponse.cs
@@ -0,0 +1,16 @@
+﻿using LearningCenter.API.Learning.Domain.Models;
+using LearningCenter.API.Security.Domain.Models;
+using LearningCenter.API.Shared.Domain.Services.Communication;
+
+namespace LearningCenter.API.Security.Domain.Services.Communication;
+
+public class RoleResponse : BaseResponse<Role>
+{
+    public RoleResponse(string message) : base(message)
+    {
+    }
+
+    public RoleResponse(Role model) : base(model)
+    {
+    }
+}
